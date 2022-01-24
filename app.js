5de6e4aa9cb2d61919efe5ca96852cf0899c3a99
govind-3000 <govindraj99.stark@gmail.com>
2022-01-24 13:01:07 +0530

    bodyparser

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(bodyParser.json());//to get json data from request body
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
+
+app.use((req,res,next)=>{
+    res.setHeader('Acces-Control-Allow-Origin','*');
+    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 //route middleware
