f331d90752c176d12fb12959ccb2efd05315bbe8
govind-3000 <govindraj99.stark@gmail.com>
2022-01-27 00:18:07 +0530

    cors

--- app.js
+++ app.js
@@ -29,15 +29,6 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
-
-// app.use((req,res,next)=>{
-//     res.setHeader('Acces-Control-Allow-Origin','*');
-//     res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-//     res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
-//     next(); 
-// })
-// app.use(bodyParser.json({limit:"30mb", extended:true}));
-// app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 //route middleware
 app.use('/', authRouter);
 app.use('/', userRouter);
